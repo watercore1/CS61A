d5b4f9410ecfc55796d813e0ac588087e2a10fa9
watercore1 <1900012476@pku.edu.cn>
2022-04-22 15:32:42 +0800

    hog

--- hws/hw02/hw02.py
+++ hws/hw02/hw02.py
@@ -98,7 +98,7 @@ def product_using_accumulate(n, term):
     True
     """
     "*** YOUR CODE HERE ***"
-    return accumulate(mul, 0, n, term)
+    return accumulate(mul, 1, n, term)
 
 
 def compose1(func1, func2):
@@ -138,12 +138,17 @@ def successor(n):
 def one(f):
     """Church numeral 1: same as successor(zero)"""
     "*** YOUR CODE HERE ***"
+    def fx(x):
+        return f(x)
+    return fx
 
 
 def two(f):
     """Church numeral 2: same as successor(successor(zero))"""
     "*** YOUR CODE HERE ***"
-
+    def ffx(x):
+        return f(f(x))
+    return ffx
 
 three = successor(two)
 
@@ -161,7 +166,9 @@ def church_to_int(n):
     3
     """
     "*** YOUR CODE HERE ***"
-
+    def add1(x):
+        return x+1
+    return n(add1)(0)
 
 def add_church(m, n):
     """Return the Church numeral for m + n, for Church numerals m and n.
@@ -170,7 +177,10 @@ def add_church(m, n):
     5
     """
     "*** YOUR CODE HERE ***"
-
+    int_n = church_to_int(n)
+    for i in range(int_n):
+        m=successor(m)
+    return m
 
 def mul_church(m, n):
     """Return the Church numeral for m * n, for Church numerals m and n.
@@ -182,7 +192,11 @@ def mul_church(m, n):
     12
     """
     "*** YOUR CODE HERE ***"
-
+    ans = zero
+    int_n = church_to_int(n)
+    for i in range(int_n):
+        ans = add_church(ans, m)
+    return ans
 
 def pow_church(m, n):
     """Return the Church numeral m ** n, for Church numerals m and n.
@@ -193,3 +207,8 @@ def pow_church(m, n):
     9
     """
     "*** YOUR CODE HERE ***"
+    ans = one
+    int_n = church_to_int(n)
+    for i in range(int_n):
+        ans = mul_church(ans, m)
+    return ans
